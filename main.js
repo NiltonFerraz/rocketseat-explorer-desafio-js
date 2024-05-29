3bdde8e014661981911c54325fd6be7931b28d87
Nilton Ferraz <nilferraz@gmail.com>
2024-05-29 07:06:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+// Exerc1
+// alert('Hello World!')
+
+//Exerc2
+// let numb1 = prompt('Digite o primeiro valor:')
+// let numb2 = prompt('Digite o segundo valor:')
+
+// let soma = Number(numb1) + Number(numb2)
+// alert(`A soma é ${soma}`) 
+
+//Exerc3
+let varInput = prompt('Digite algo:')
+
+if ( Number(varInput) == varInput)
+    alert('É numérico')
+else
+    alert('Não é numéricol')
+
+//Exerc4
+// let varInput = prompt('Digite algo:')
+
+// if (String(varInput) == varInput)
+
+// if (varInput instanceof String || typeof varInput === 'string')
+//     alert('É uma string')
+// else
+//     alert('Não é uma string')
+
+
+//Exerc5
+// let varInput = prompt('Digite algo:')
+// if (varInput === 'true' || varInput === 'false')
+//     alert('É um boolean')
+// else
+//     alert('Não é um boolean')
+
+//Exerc6
+// let numb1 = prompt('Digite o primeiro valor:')
+// let numb2 = prompt('Digite o segundo valor:')
+
+// let sub = Number(numb1) - Number(numb2)
+// alert(`A subtração é ${sub}`)
+
+//Exerc7
+// let numb1 = prompt('Digite o primeiro valor:')
+// let numb2 = prompt('Digite o segundo valor:')
+
+// let mult = Number(numb1) * Number(numb2)
+// alert(`A multiplicação é ${mult}`)
+
+//Exerc8
+// let numb1 = prompt('Digite o primeiro valor:')
+// let numb2 = prompt('Digite o segundo valor:')
+
+// let div = Number(numb1) / Number(numb2)
+// alert(`A divisão é ${div}`)
+
+//Exerc9
+// let variable = prompt('Digite um número:')
+// let result = Number(variable)
+
+// if(result % 2 == 0)
+//     alert(`O valor ${result} é par`)
+// else
+//     alert(`O valor ${result} é ímpar`)
